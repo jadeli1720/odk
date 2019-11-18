db40d463e5f33e098a2adc7e63b30c3ce5cb4740
Jade E Lopez <38263897+jadeli1720@users.noreply.github.com>
2019-11-17 19:19:10 -0700

    Added relevant seeds and knex-cleaner

---
+++ data/seeds/villages.js
@@ -0,0 +1,203 @@
+
+exports.seed = function(knex) {
+  return knex("village").insert([
+    {
+      id: 17,
+      name: "Bugambo",
+      latitude: "0.8970166000000022",
+      longitude: "33.39893489999997"
+    },
+    {
+      id: 29,
+      name: "Buganza",
+      latitude: "0.8453280000000231",
+      longitude: "33.433343899999954"
+    },
+    {
+      id: 1,
+      name: "Bugole A",
+      latitude: "0.8683849999999976",
+      longitude: "33.382454999999936"
+    },
+    {
+      id: 2,
+      name: "Bugole B",
+      latitude: "0.8527656000000022",
+      longitude: "33.373872000000006"
+    },
+    {
+      id: 3,
+      name: "Bugongo A",
+      latitude: "0.8630280000000112",
+      longitude: "33.39781700000003"
+    },
+    {
+      id: 10,
+      name: "Bugongo B",
+      latitude: "0.8541590000000057",
+      longitude: "33.39529300000004"
+    },
+    {
+      id: 16,
+      name: "Bukyeega",
+      latitude: "0.7941138000000139",
+      longitude: "33.4319706"
+    },
+    {
+      id: 18,
+      name: "Bunyokano",
+      latitude: "0.8673802000000016",
+      longitude: "33.4082042"
+    },
+    {
+      id: 13,
+      name: "Busaala",
+      latitude: "0.808081400000012",
+      longitude: "33.42802240000003"
+    },
+    {
+      id: 9,
+      name: "Busakaire",
+      latitude: "0.879166000000019",
+      longitude: "33.38932180000006"
+    },
+    {
+      id: 19,
+      name: "Busoigi",
+      latitude: "0.8786537999999963",
+      longitude: "33.40477099999998"
+    },
+    {
+      id: 14,
+      name: "Buwumba",
+      latitude: "0.8285069999999863",
+      longitude: "33.4343738"
+    },
+    {
+      id: 25,
+      name: "Buyunga",
+      latitude: "0.863843000000015",
+      longitude: "33.428709000000026"
+    },
+    {
+      id: 20,
+      name: "Buzaya",
+      latitude: "0.8910460000000094",
+      longitude: "33.41290600000002"
+    },
+    {
+      id: 21,
+      name: "Ikumbya",
+      latitude: "0.8779671999999938",
+      longitude: "33.44116329999997"
+    },
+    {
+      id: 22,
+      name: "Kabuli 1",
+      latitude: "0.8784890000000011",
+      longitude: "33.427318000000014"
+    },
+    {
+      id: 23,
+      name: "Kabuli 2",
+      latitude: "0.8766488999999923",
+      longitude: "33.416100700000015"
+    },
+    {
+      id: 30,
+      name: "Kiringa A",
+      latitude: "0.8329696999999938",
+      longitude: "33.420125900000016"
+    },
+    {
+      id: 31,
+      name: "Kiringa B",
+      latitude: "0.8134022999999982",
+      longitude: "33.417551"
+    },
+    {
+      id: 11,
+      name: "Kiwanyi 1",
+      latitude: "0.8190666000000089",
+      longitude: "33.433343899999954"
+    },
+    {
+      id: 12,
+      name: "Kiwanyi 2",
+      latitude: "0.7989198000000097",
+      longitude: "33.422872600000005"
+    },
+    {
+      id: 24,
+      name: "Kyendabawala",
+      latitude: "0.866638900000017",
+      longitude: "33.426228700000024"
+    },
+    {
+      id: 15,
+      name: "Madhimasu",
+      latitude: "0.8059572999999999",
+      longitude: "33.44055370000001"
+    },
+    {
+      id: 26,
+      name: "Malobi A",
+      latitude: "0.871444800000015",
+      longitude: "33.45455279999999"
+    },
+    {
+      id: 27,
+      name: "Malobi B",
+      latitude: "0.871444800000015",
+      longitude: "33.45455279999999"
+    },
+    {
+      id: 4,
+      name: "Namabwere 1",
+      latitude: "0.8272050000000034",
+      longitude: "33.39704000000006"
+    },
+    {
+      id: 5,
+      name: "Namabwere 2",
+      latitude: "0.8388055999999934",
+      longitude: "33.40690799999993"
+    },
+    {
+      id: 6,
+      name: "Namakumya",
+      latitude: "0.8532236000000194",
+      longitude: "33.40768920000005"
+    },
+    {
+      id: 28,
+      name: "Namusiisi",
+      latitude: "0.8570650000000077",
+      longitude: "33.43251499999997"
+    },
+    {
+      id: 7,
+      name: "Nawandala",
+      latitude: "0.835544400000004",
+      longitude: "33.38321870000004"
+    },
+    {
+      id: 32,
+      name: "Nawangaiza 1",
+      latitude: "0.808081400000012",
+      longitude: "33.40484809999998"
+    },
+    {
+      id: 8,
+      name: "Nawangaiza 2",
+      latitude: "0.8061933000000043",
+      longitude: "33.390428499999985"
+    },
+    {
+      id: 99,
+      name:"Other",
+      latitude:"",
+      longitude: "",
+    }
+  ]);
+};
